f92c85e7fc94e724b47bd473bdb66b75a47b6e76
zahras09 <zahras09@gmail.com>
2016-08-29 00:32:29 +0000

    Assessment #6

---
+++ AssessmentPart_1.py
@@ -0,0 +1,50 @@
+Runtime:
+1.
+2. 
+•	O(1)
+•	O(logn)
+•	O(n)
+•	O(nlogn)
+•	O(n^2)
+•	O(2^n)
+Stacks and Queue:
+1.	Stacks
+2.	Queue
+ 
+2.	Queue: Situations where you need to maintain a FIFO order
+3.	Stacks: Used for the undo buttons
+
+Linked Lists:
+1.	Nodes are: base units; Apple and next, Berry and next, Cherry and next. Data for each node is a string. The head is: LLIST. The tail is the last node in the list which is “Cherry and next”. The arrows are used to reference the next node.
+
+2.	A doubly linked list is a list that has two references, one to the next node and another to previous node. And singly lined lists have one reference which is the next node.
+
+3.	If you keep track of the tail then there’s no need to iterate through the whole list and find the end and then add the item to the end.
+
+
+Trees:
+
+1. Depth First Search:
+[food]
+[italian, indian, mexican]
+[italian, indian, burrito, tacos, enchiladas]
+[italian, indian, burrito, tacos]
+[italian, indian, burrito]
+
+2. Breadth First Search:
+[food]
+[italian, indian, mexican]
+[indian, mexican, lasagna, pizza]
+[mexican, lasagna, pizza, tikka masala, saag]
+[lasagna, pizz, tikka masala, saag, burrito, tacos, enchiladas]
+[pizza, tikka masala, saag, burrito, tacos, enchiladas]
+[tikka masala, saag, burrito, tacos, enchiladas, thin-crust, chicago-style, new-york-style, sicilian]
+[saag, burrito, tacos, enchiladas, thin-crust, chicago-style, new-york-style, sicilian]
+[burrito, tacos, enchiladas, thin-crust, chicago-style, new-york-style, sicilian]
+[tacos, enchiladas, thin-crust, chicago-style, new-york-style, sicilian]
+[enchiladas, thin-crust, chicago-style, new-york-style, sicilian]
+[thin-crust, chicago-style, new-york-style, sicilian]
+[chicago-style, new-york-style, sicilian]
+
+3. Binary search tree is sorted and has a rule for arrangement. 
+
